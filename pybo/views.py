a0a8749f54a2a73a5bde7225dc5bcf6c167f9d7d
GJ <lgj9172@gmail.com>
2021-05-23 00:16:33 +0900

    add: 2-06

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
+from django.utils import timezone
 
 def index(request):
-    return HttpResponse("안녕하세요 파이보에 오신것은 환영합니다.")
+    """
+    파이보 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    # return HttpResponse("안녕하세요 파이보에 오신것은 환영합니다.")
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    파이보 내용 출력
+    """
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
