65fc16b512262f7a54261c7d5d6e6c209a70ed66
GJ <lgj9172@gmail.com>
2021-06-21 14:51:20 +0900

    add: 3-11

--- pybo/models.py
+++ pybo/models.py
@@ -7,7 +7,8 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
@@ -17,7 +18,8 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
